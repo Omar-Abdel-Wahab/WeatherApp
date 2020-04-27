54087fceba4132ab2cadce5c978dc1e496d90e2d
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2020-04-27 23:27:54 +0200

    GUI is okay. Used grid system instead of packing, and also made some unit conversions

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import requests, json
 root = Tk()
 root.title("Weather App")
 root.iconbitmap("weather.ico")
-root.geometry("600x400")
+root.geometry("502x320")
 root.configure(bg="#FFFFFF")
 root.resizable(False, False)
 
@@ -15,20 +15,24 @@ img = Image.open("weather_og.png")
 img = img.resize((500, 200))
 img = ImageTk.PhotoImage(img)
 img_label = Label(root, image=img)
-#img_label.grid(row=0, column=1, columnspan=3)
-img_label.pack()
+img_label.grid(row=0, column=0, columnspan=11)
+# img_label.pack()
+
+city_label = Label(root, text="City: ", bg="#FFFFFF")
+city_label.grid(row=1, column=4)
 
 # Create an entry, adjust its properties and placement
 city_var = StringVar()
 city_var.set("Enter city name")
 city_entry = Entry(root, textvariable=city_var, fg="#808080")
-#city_entry.grid(row=1, column=1, columnspan=3)
-city_entry.pack()
+city_entry.grid(row=1, column=5)
+# city_entry.pack()
 
 
 def clear_entry(event):
     """Clear the entry upon mouse click"""
     city_var.set("")
+    city_entry.configure(fg="#000000")
 
 
 # Add mouse-click event
@@ -44,21 +48,45 @@ def get_weather():
     api_key = "c64618b984f6134b0d2f88e7b828cf8d"
     city_name = city_entry.get()
     complete_url = base_url + "q=" + city_name + "&appid=" + api_key
-    try:
+    if["cod"] != '404':
         response = requests.get(complete_url)
         json_data = response.json()
-        weather_label.configure(text=json_data["main"]["temp"])
-    except requests.exceptions.RequestException as e:
-        response = "Error"
-    except ValueError as e:
+        temperature = json_data["main"]["temp"]
+        temperature = round(float(temperature) - 273.15, 2)
+        pressure = json_data["main"]["pressure"]
+        description = json_data["weather"][0]["description"].title()
+        temperature_value_label.configure(text=str(temperature) + " C")
+        pressure_value_label.configure(text=str(pressure) + " Pascal")
+        description_value_label.configure(text=description)
+    else:
         json_data = "Error"
+        response = "Error"
 
 
 get_button = Button(root, text="Get Weather", command=get_weather)
-get_button.pack()
+get_button.grid(row=2, column=5)
+# get_button.pack()
 
-weather_label = Label(root, text="Temperature here")
-weather_label.pack()
+temperature_label = Label(root, text="Temperature: ", bg="#FFFFFF")
+temperature_label.grid(row=3, column=4)
+
+temperature_value_label = Label(root, text="Temperature here", bg="#FFFFFF")
+temperature_value_label.grid(row=3, column=6)
+# temperature_value_label.pack()
+
+pressure_label = Label(root, text="Pressure: ", bg="#FFFFFF")
+pressure_label.grid(row=4, column=4)
+
+pressure_value_label = Label(root, text="Pressure here", bg="#FFFFFF")
+pressure_value_label.grid(row=4, column=6)
+# pressure_value_label.pack()
+
+description_label = Label(root, text="Description: ", bg="#FFFFFF")
+description_label.grid(row=5, column=4)
+
+description_value_label = Label(root, text="Description here", bg="#FFFFFF")
+description_value_label.grid(row=5, column=6)
+# description_value_label.pack()
 
 # Keep the root window running
 root.mainloop()
