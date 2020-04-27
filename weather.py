3d21fa08b1166f4447c2411b28c08494ba6eeac1
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2020-04-27 23:56:46 +0200

    Added comments for future lookup and adjusted a small bug

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
-import requests, json
+import requests
 
 # Creating the root window, and adding some properties
 root = Tk()
@@ -12,16 +12,19 @@ root.resizable(False, False)
 
 
 def center(window):
+    """Center a Tkinter window"""
     window.update_idletasks()
 
-    # Tkinter way to find the screen resolution
+    # Find the screen resolution
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
 
+    # Find new (x, y) coordinates
     size = tuple(int(_) for _ in window.geometry().split('+')[0].split('x'))
     x = screen_width/2 - 5 * size[0] / 9
     y = screen_height/2 - 3 * size[1] / 5
 
+    # Apply new coordinates
     window.geometry("+%d+%d" % (x, y))
 
 
@@ -33,8 +36,8 @@ img = img.resize((500, 200))
 img = ImageTk.PhotoImage(img)
 img_label = Label(root, image=img)
 img_label.grid(row=0, column=0, columnspan=11)
-# img_label.pack()
 
+# Create a label for the city name
 city_label = Label(root, text="City: ", bg="#FFFFFF")
 city_label.grid(row=1, column=4)
 
@@ -43,7 +46,6 @@ city_var = StringVar()
 city_var.set("Enter city name")
 city_entry = Entry(root, textvariable=city_var, fg="#808080")
 city_entry.grid(row=1, column=5)
-# city_entry.pack()
 
 
 def clear_entry(event):
@@ -56,18 +58,24 @@ def clear_entry(event):
 city_entry.bind("<Button-1>", clear_entry)
 
 # API Request
-# api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+# https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
 
 def get_weather():
-
+    """Get weather related data to show on the screen"""
+    # URL parts
     base_url = "https://api.openweathermap.org/data/2.5/weather?"
     api_key = "c64618b984f6134b0d2f88e7b828cf8d"
     city_name = city_entry.get()
     complete_url = base_url + "q=" + city_name + "&appid=" + api_key
+
+    # Create a GET request and parse it as JSON
+    response = requests.get(complete_url)
+    json_data = response.json()
+
+    # If the request was a success
     if["cod"] != '404':
-        response = requests.get(complete_url)
-        json_data = response.json()
+        # Get the weather data, apply conversions or functions
         temperature = json_data["main"]["temp"]
         temperature = round(float(temperature) - 273.15, 2)
         pressure = json_data["main"]["pressure"]
@@ -80,30 +88,28 @@ def get_weather():
         response = "Error"
 
 
+# Create a button to search by city name
 get_button = Button(root, text="Get Weather", command=get_weather)
 get_button.grid(row=2, column=5)
-# get_button.pack()
 
+# Create labels for temperature, pressure, and description
 temperature_label = Label(root, text="Temperature: ", bg="#FFFFFF")
 temperature_label.grid(row=3, column=4)
 
 temperature_value_label = Label(root, text="Temperature here", bg="#FFFFFF")
 temperature_value_label.grid(row=3, column=6)
-# temperature_value_label.pack()
 
 pressure_label = Label(root, text="Pressure: ", bg="#FFFFFF")
 pressure_label.grid(row=4, column=4)
 
 pressure_value_label = Label(root, text="Pressure here", bg="#FFFFFF")
 pressure_value_label.grid(row=4, column=6)
-# pressure_value_label.pack()
 
 description_label = Label(root, text="Description: ", bg="#FFFFFF")
 description_label.grid(row=5, column=4)
 
 description_value_label = Label(root, text="Description here", bg="#FFFFFF")
 description_value_label.grid(row=5, column=6)
-# description_value_label.pack()
 
 # Keep the root window running
 root.mainloop()
